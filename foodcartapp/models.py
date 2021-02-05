5ca444902018187fe68d23b07fb8202527798e58
axmetES <fraktsia@gmail.com>
2021-02-05 13:06:31 +0600

    Added link to admin edit page. Added choices field order_status to order model.

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -73,10 +73,17 @@ class RestaurantMenuItem(models.Model):
 
 
 class Order(models.Model):
+    PROCESSED = 'Processed'
+    UNPROCESSED = 'Unprocessed'
+    STATUS = [
+        ('Обработанный', 'Processed'),
+        ('Необработанный', 'Unprocessed')
+    ]
     firstname = models.CharField(max_length=50, verbose_name='имя')
     lastname = models.CharField(max_length=50, verbose_name='фамилия')
     address = models.CharField(max_length=100, verbose_name='адрес')
     phonenumber = PhoneNumberField(verbose_name='номер телефона')
+    order_status = models.CharField(max_length=14, choices=STATUS, default=UNPROCESSED, verbose_name='статус заказа')
 
     def __str__(self):
         return self.firstname
